eb34517ef1b5674e099854c23f85e350475440bf
chechojgb <100326731+chechojgb@users.noreply.github.com>
2025-01-22 17:07:25 -0500

    Add: Search input
    Se agrego el input de busqueda de la informacion de cada campaign, esta busqueda ya funciona sin que interceda el refresco, cuando se refresca se pierde lo que se escribio en el input

--- resources/views/components/select-campaign.blade.php
+++ resources/views/components/select-campaign.blade.php
@@ -1,4 +1,4 @@
-<div class="w-full max-w-md mx-auto">
+{{-- <div class="w-full max-w-md mx-auto">
     <div class="relative">
         <select 
             class="block w-full bg-gray-100 text-gray-800 border border-transparent rounded-t-lg shadow-sm focus:ring focus:ring-blue-300 focus:outline-none focus:border-blue-500 transition appearance-none p-3 pr-10" 
@@ -13,5 +13,23 @@
             @endforeach
         </select>
     </div>
-</div>
+</div> --}}
 
+<div class="w-full max-w-md mx-auto">
+    <div class="relative">
+        <select 
+            class="block w-full bg-gray-100 text-gray-800 border border-transparent rounded-t-lg shadow-sm focus:ring focus:ring-blue-300 focus:outline-none focus:border-blue-500 transition appearance-none p-3 pr-10" 
+            style="border-bottom: 1px solid black" 
+            name="{{ $name }}">
+            <!-- Título como opción deshabilitada -->
+            <option value="" disabled selected hidden>{{ __($title) }}</option>
+
+            <!-- Opciones dinámicas -->
+            @foreach ($options as $value => $label)
+                <option value="{{ $value + 1}}" @selected(session('campaignIndex') == $value)>
+                    {{ __($label) }}
+                </option>
+            @endforeach
+        </select>
+    </div>
+</div>
